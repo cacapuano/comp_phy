d2127ebfc302d4e6fba1ea9efcb3991f1792ab0c
Caroline Capuano <carolinecapuano@MacBook-Air-2.local>
2024-09-19 13:08:50 -0400

    project_1_main.py

--- project_1_main.py
+++ project_1_main.py
@@ -1,1 +1,13 @@
-blah blah
+#just importing packages
+from matplotlib import pyplot
+import numpy 
+
+
+#constants
+mass = 10 #kg; mass on the pendulum
+length = 1 #m; length of the string
+g = 9.81 #m/s; gravity!
+init_theta = np.pi/4 #initial angle
+init_omega = 0.0 #starting velocity
+
+
