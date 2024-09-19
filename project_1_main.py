8783ee65312bfa76ca64bfb824434f601edda52f
Caroline Capuano <carolinecapuano@MacBook-Air-2.local>
2024-09-19 13:17:34 -0400

    project_1_main.py

--- project_1_main.py
+++ project_1_main.py
@@ -10,4 +10,11 @@ g = 9.81 #m/s; gravity!
 init_theta = np.pi/4 #initial angle
 init_omega = 0.0 #starting velocity
 
+#initial conditions
+theta_val[0] = init_theta
+omega_val[0] = init_omega
+
+for i in range(steps -1):
+    theta = theta_val[i]
+    omega = omega_val[i]
 
