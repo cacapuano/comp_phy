a78d628b9c9e7a0ac37a9fe0fb7c147f2a78abc3
Caroline Capuano <carolinecapuano@MacBook-Air-2.lan>
2024-09-24 18:16:43 -0400

    pendulum_euler.py

--- pendulum_euler.py
+++ pendulum_euler.py
@@ -8,24 +8,24 @@ import matplotlib.pyplot as plt
 mass = 10 #kg; mass on the pendulum
 length = 1 #m; length of the string
 g = 9.81 #m/s^2; gravity!
-init_theta = np.pi/4 #initial angle
-init_omega = 0.0 #starting velocity
+init_theta = np.pi/4 #initial angle radians
+init_omega = 0.0 #starting velocity rad/s
 
 #time parameters
 dt = 0.01
-max_time = 20
+max_time = 20 #seconds
+
+#arrays
+time_val = np.arange(0, max_time, dt) #time array
+theta_val = []
+omega_val = []
 
 #initial conditions
 theta_val[0] = init_theta
 omega_val[0] = init_omega
 
-#arrays
-time_val = np.arange(0, max_time, dt) #time array
-theta_val = [] 
-omega_val = []
-
 #euler's method to update theta and omega
 
-for i in range(1, len(time_val)):
-    
-
+for i in range(1, len(time_val)):    
+    theta_values[i + 1] = init_omega + dt * omega_val[i]
+    omega_values[i + 1] = init_theta + dt * (-g / length * np.sin(theta_val[i]))
