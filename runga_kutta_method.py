c9639b7e5d3026719e421082b093a99393933482
Caroline Capuano <carolinecapuano@MacBook-Air-2.local>
2024-09-26 11:01:28 -0400

    added equations for the pendulum

--- runga_kutta_method.py
+++ runga_kutta_method.py
@@ -1,8 +1,7 @@
 # runga kutta method of intergration
 #just importing packages
 from matplotlib import pyplot
-import numpy 
-
+import numpy as np
 
 #constants
 mass = 10 #kg; mass on the pendulum
@@ -10,6 +9,15 @@ length = 1 #m; length of the string
 g = 9.81 #m/s^2; gravity!
 init_theta = np.pi/4 #initial angle
 init_omega = 0.0 #starting velocity
+
+def pendulum_functions(t, position):
+    theta = position
+    omega = position
+    dtheta_dt = omega
+    domega_dt = -(g / length) * np.sin(theta)
+    
+    return np.array([dtheta_dt, domega_dt])
+    
 
 #initial conditions
 theta_val[0] = init_theta
@@ -47,3 +55,24 @@ for i in range(steps -1):
     theta_val[i + 1] = theta + (1/6) * (k1_theta + 2*k2_theta + 2*k3_theta + k4_theta)
     omega_val[i + 1] = omega + (1/6) * (k1_omega + 2*k2_omega + 2*k3_omega + k4_omega)
 
+
+# Angle plot
+plt.subplot(2, 1, 1)
+plt.plot(t_values, theta_values, label='Angle (theta)', color='blue')
+plt.title('Pendulum Motion')
+plt.xlabel('Time (s)')
+plt.ylabel('Angular Position (radians)')
+plt.grid()
+plt.legend()
+
+# Angular velocity plot
+plt.subplot(2, 1, 2)
+plt.plot(t_values, omega_values, label='Angular Velocity (omega)', color='orange')
+plt.title('Pendulum Velocity')
+plt.xlabel('Time (s)')
+plt.ylabel('Angular Velocity (rad/s)')
+plt.grid()
+plt.legend()
+
+plt.tight_layout()
+plt.show()
