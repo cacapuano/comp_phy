ecc624227033d2e1ce898a21bea606bc524865f6
Caroline Capuano <carolinecapuano@MacBook-Air-2.local>
2024-09-26 10:49:04 -0400

    cleaned up code and comments

--- integral_scipy.py
+++ integral_scipy.py
@@ -3,9 +3,26 @@
 from scipy.integrate import quad
 import numpy as np
 
-def integrand(r, a, b):
-    epislon_0 = 8.85e-12
-    return (epsilon_0/2)((1 / (4 * np.pi * epsilon_0)) * (q / r**2)**2)
+#constants
+e_0 = 8.85e-12 #C^2/Nm^2 permittivity of free space
+q = 1e-6 # coulombs - charge of the conducting sphere
+R = 3e-3 # radius of the sphere m
+a = R #start integration at the radius of the sphere
+b = 3 # m
+
+# defining function that we will be integrating over to find the work done
+def work(r):
+    if r < R:
+        return 0 # electric field will be zero inside sphere
+    e_0 = 8.85e-12
+    top = e_0/2
+    k = 1 / (4 * np.pi * e_0)
+    E_field = (k * q / r**2)**2
+    return top * E_field
 
 
-I = quad(integrand, 1, 10, args=())
+# integrate function
+Work_integrated_SciPy, error = quad(work, a, b)
+
+print(f"Work done to move the conducting sphere from {R} m to {b} m using SciPy: {Work_integrated_SciPy:e} N*m/C")
+
