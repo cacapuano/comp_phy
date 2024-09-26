b43895f23f92e021ba0a3c53a0453ccc78a273b8
Caroline Capuano <carolinecapuano@MacBook-Air-2.lan>
2024-09-25 20:21:35 -0400

    simpsons_rule.py

--- simpsons_rule.py
+++ simpsons_rule.py
@@ -9,32 +9,42 @@ R = 3e-3 # radius of the sphere m
 # Simpson's Rule parameters
 a = 1          # Start of the interval
 b = 3          # End of the interval
-n = 1000       # Number of subintervals (must be even)
+interval = 1000       # Number of subintervals (must be even)
 
 
 #work done moving conducting sphere integral
-def work(r,q):
+def work(r):
     e_0 = 8.85e-12
-    return (e_0 / 2)((1 / (4 * np.pi * e_0)) * (q / r**2)**2)
+    top = e_0/2
+    k = 1 / (4 * np.pi * e_0)
+    E_field = (k * q / r**2)**2
+    return top * E_field
+  
 
+def simpsons_rule(function, a, b, interval):
 
 # Ensure n is even
-if n % 2 == 1:
-    n += 1
+    if interval % 2 == 1:
+        interval += 1
 
 # Calculate the width of each subinterval
-h = (b - a) / n
+    h = (b - a) / interval
 
 # Simpson's Rule calculation
-integral = work(a) + work(b)
+    integral = function(a) + function(b)
 
-for i in range(1, n, 2):
-    integral += 4 * work(a + i * h)
+    for i in range(1, interval, 2):
+        integral += 4 * function(a + i * h)
 
-for i in range(2, n-1, 2):
-    integral += 2 * work(a + i * h )
+    for i in range(2, interval-1, 2):
+        integral += 2 * function(a + i * h )
 
-integral *= h / 3
+    integral *= h / 3
+    return integral
+
+# Integrate
+Work_integrated_simpson = simpsons_rule(work, a, b, interval)
+
 
 # Print the result
-print(f"Approximate work done moving the conducting sphere from {a} to {b} using Simpson's Rule: {integral}")
+print(f"Approximate work done moving the conducting sphere from {a} to {b} using Simpson's Rule: {Work_integrated_simpson:.2e} N*m/C")
