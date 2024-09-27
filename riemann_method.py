b50f72b17f99f66537ba317e59f9dbb3c0ca3f7d
Caroline Capuano <carolinecapuano@MacBook-Air-2.lan>
2024-09-27 17:41:05 -0400

    riemann_method.py

--- riemann_method.py
+++ riemann_method.py
@@ -7,7 +7,7 @@ e_0 = 8.85e-12 #C^2/Nm^2 permittivity of free space
 q = 1e-6 # coulombs - charge of the conducting sphere
 R = 3e-3 # radius of the sphere m
 
-# Riemann Sum parameters
+# riemann Sum parameters
 a = R          # Start of the interval
 b = 3          # End of the interval
 interval = 1000       # Number of subintervals
@@ -23,26 +23,18 @@ def work(r):
 
 # riemann's sum definition
 def riemann_sum(function, a, b, interval,  method = 'left'):
-    h = (b - a) / interval  # Step size
+    h = (b - a) / interval  # step size
     total_area = 0
 
     for i in range(interval):
-        if method == 'left':
-            x = a + i * h  # Left endpoint
-        elif method == 'right':
-            x = a + (i + 1) * h  # Right endpoint
-        elif method == 'mid':
-            x = a + (i + 0.5) * h  # Midpoint
-        else:
-            raise ValueError("Method must be 'left', 'right', or 'mid'")
-
+        x = a + i * h  # using left endpoint method
         total_area += function(x) * h  # Area of the rectangle
 
     return total_area
 
 
 # integrate
-Work_integrated_riemann = riemann_sum(work, a, b, interval, method='left')
+Work_integrated_riemann = riemann_sum(work, a, b, interval)
 
 
 # Print the result
