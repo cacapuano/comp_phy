8005f1ec859cbb554219db6a02e7c44469042c1e
Caroline Capuano <carolinecapuano@MacBook-Air-2.lan>
2024-09-27 21:25:20 -0400

    final comment

--- trap_method.py
+++ trap_method.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 #constants
 e_0 = 8.85e-12 #C^2/Nm^2 permittivity of free space
 q = 1e-6 # coulombs - charge of the conducting sphere
-R = 3e-3 # (m) radius of the sphere 
+R = 3e-3 # m radius of the sphere 
 
 #integration parameters
 a = R #lower limit of integration
-b = 3 #(m) upper limit of integration
+b = 3 # m upper limit of integration
 intervals = 1000 #steps
 
 # defining function to integrate
@@ -20,7 +20,7 @@ def work(r):
     k = 1 / (4 * np.pi * e_0)
     E_field = (k * q / r**2)**2
     return top * E_field
-#work(r)
+
 
 # trapezoidal rule method of integration
 def trapezoidal_rule(function, a, b, intervals):
