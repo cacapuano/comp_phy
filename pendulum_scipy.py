eeec687bff7b0c9c5892d7fc2be1cc95a95ee51d
Caroline Capuano <carolinecapuano@MacBook-Air-2.lan>
2024-09-27 21:16:50 -0400

    edited graph and made more comments

--- pendulum_scipy.py
+++ pendulum_scipy.py
@@ -5,14 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-# Constants
+# constants
 g = 9.81  # m/s^2 gravity
-L = 1.0   # m length of the pendulum 
+length = 1.0   # m length of the pendulum 
 
-# system of equations
+# equations
 def pendulum_scipy(time, y):
-    theta, omega = y  
-    dydt = [omega, -g / L * np.sin(theta)]  # [dtheta/dt, domega/dt]
+    theta, omega = y #array for theta and omega values  
+    dydt = [omega, -g / length * np.sin(theta)]  # [dtheta/dt, domega/dt] #derivative function
     return dydt
 
 # initial conditions
@@ -20,24 +20,28 @@ init_theta = np.pi / 4  # initial angle
 init_omega = 0.0        # initial angular velocity
 y_0_scipy = [init_theta, init_omega]
 
-time_span = (0, 10) 
-time_eval = np.linspace(time_span[0], time_span[1], 10) 
+#time of function
+time_span = (0, 10) #seconds 
+#time_eval = np.linspace(time_span[0], time_span[1], 10) #evaluation time for package 
 
-solution = solve_ivp(pendulum_scipy, time_span, y_0_scipy, time_eval=time_eval)
+solution = solve_ivp(pendulum_scipy, time_span, y_0_scipy)
 
 # Plot the results
-plt.figure(figsize=(10, 4))
+plt.figure(figsize=(12, 6))
+plt.subplot(2, 1, 1)
 plt.plot(solution.t, solution.y[0], label='Angular Position', color='blue')
 plt.title('Pendulum Motion')
 plt.xlabel('Time (s)')
 plt.ylabel('Angular Position (rad)')
 plt.legend()
 
-plt.figure(figsize=(10, 4))
+plt.subplot(2, 1, 2)
 plt.plot(solution.t, solution.y[1], label='Angular Velocity (omega)', color='orange')
 plt.title('Pendulum Velocity')
 plt.xlabel('Time (s)')
 plt.ylabel('Angular Velocity (rad/s)')
 plt.legend()
 plt.grid()
+
+plt.tight_layout()
 plt.show()
