fccc5e20c7fda70ffc3e5f8edfa885cc5db3e403
Caroline Capuano <carolinecapuano@MacBook-Air-2.lan>
2024-09-27 21:34:10 -0400

    cleaned up code

--- integral_scipy.py
+++ integral_scipy.py
@@ -5,15 +5,13 @@ import numpy as np
 
 #constants
 e_0 = 8.85e-12 #C^2/Nm^2 permittivity of free space
-q = 1e-6 # coulombs - charge of the conducting sphere
+q = 4e-6 # coulombs - charge of the conducting sphere
 R = 3e-3 # radius of the sphere m
 a = R #start integration at the radius of the sphere
 b = 3 # m
 
 # defining function that we will be integrating over to find the work done
 def work(r):
-    if r < R:
-        return 0 # electric field will be zero inside sphere
     e_0 = 8.85e-12
     top = e_0/2
     k = 1 / (4 * np.pi * e_0)
